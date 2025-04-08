5c3f671057b2bce3d4ac551a89bd460bb6aa8e48
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2025-04-08 19:08:28 -0400

    fix(kubejs): make some slab recipes actually work

--- kubejs/server_scripts/slabs.js
+++ kubejs/server_scripts/slabs.js
@@ -1,47 +1,49 @@
-ServerEvents.recipes(event => {
-	function compact(a, b) {
+const acq = []
+function compact(a, b) {
+	acq.push(a)
+	ServerEvents.recipes(event => {
 		event.recipes.create.compacting([b], [`2x ${a}`])
-	}
-	//
-	//{{{1 wooden slabs
-	for (let wood of [
-		"minecraft:oak",
-		"minecraft:spruce",
-		"minecraft:birch",
-		"minecraft:jungle",
-		"minecraft:acacia",
-		"minecraft:cherry",
-		"minecraft:dark_oak",
-		"minecraft:mangrove",
-		"minecraft:bamboo",
-		"minecraft:crimson",
-		"minecraft:warped",
-	]) {
-		compact(`${wood}_slab`, `${wood}_planks`)
-	}
-	//{{{1 pluralized slabs
-	for (let root of [
-		"minecraft:brick",
-		"minecraft:stone_brick",
-		"minecraft:mud_brick",
-		"minecraft:nether_brick",
-		"minecraft:prismarine_brick_slab",
-	]) {
-		compact(`${root}_slab`, `${root}s`)
-	}
-	//{{{1 non-pluralized slabs
-	for (let root of [
-		"minecraft:stone_slab",
-		"minecraft:smooth_stone_slab",
-		"minecraft:sandstone_slab",
-		"minecraft:cut_sandstone_slab",
-		"minecraft:granite_slab",
-		"minecraft:andesite_slab",
-		"minecraft:diorite_slab",
-		"minecraft:prismarine_slab",
-		"minecraft:cobblestone_slab",
-	]) {
-		compact(`${root}_slab`, `${root}`)
-	}
-	//}}}1
-})
+	})
+}
+//{{{1 wooden slabs
+for (let wood of [
+	"minecraft:oak",
+	"minecraft:spruce",
+	"minecraft:birch",
+	"minecraft:jungle",
+	"minecraft:acacia",
+	"minecraft:cherry",
+	"minecraft:dark_oak",
+	"minecraft:mangrove",
+	"minecraft:bamboo",
+	"minecraft:crimson",
+	"minecraft:warped",
+]) {
+	compact(`${wood}_slab`, `${wood}_planks`)
+}
+//{{{1 pluralized slabs
+for (let root of [
+	"minecraft:brick",
+	"minecraft:stone_brick",
+	"minecraft:mud_brick",
+	"minecraft:nether_brick",
+	"minecraft:prismarine_brick",
+]) {
+	compact(`${root}_slab`, `${root}s`)
+}
+//{{{1 non-pluralized slabs
+for (let root of [
+	"minecraft:stone",
+	"minecraft:smooth_stone",
+	"minecraft:sandstone",
+	"minecraft:cut_sandstone",
+	"minecraft:granite",
+	"minecraft:andesite",
+	"minecraft:diorite",
+	"minecraft:prismarine",
+	"minecraft:cobblestone",
+]) {
+	compact(`${root}_slab`, `${root}`)
+}
+//}}}1
+console.log("Known slabs: " + acq)
