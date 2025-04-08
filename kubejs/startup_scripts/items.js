8492315f9d32d3adb1ef99d8d3521d5bde5904a5
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2025-04-08 19:07:49 -0400

    feat(kubejs): powdered items & textures

---
+++ kubejs/startup_scripts/items.js
@@ -0,0 +1,5 @@
+StartupEvents.registry("item", (event) => {
+  event.create("powdered_coal").displayName("Powdered Coal");
+  event.create("powdered_zinc").displayName("Powdered Zinc");
+  event.create("wood_chips").displayName("Wood Chips");
+});
