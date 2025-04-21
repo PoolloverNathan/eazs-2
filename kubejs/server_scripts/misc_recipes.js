5dfad4b8647d79eb745353d164958a6cda1fabe8
XanderCreates <69139978+XanderCreates@users.noreply.github.com>
2025-04-21 15:15:41 -0400

    feat(kubejs): Sequenced Botany Hopper Pot Recipe

--- kubejs/server_scripts/misc_recipes.js
+++ kubejs/server_scripts/misc_recipes.js
@@ -5,4 +5,21 @@ ServerEvents.recipes(event => {
 	event.recipes.create.mixing(["kubejs:rubber_patch"], ["minecraft:paper", Fluid.of("kubejs:resin", 125)]).heated()
 	event.smelting("supplementaries:ash", "kubejs:wood_chips")
 	event.smoking("supplementaries:ash", "kubejs:wood_chips")
+	let inter = 'kubejs:hopper_pot_incomplete'
+	event.recipes.create.sequenced_assembly([
+		Item.of('botanypots:terracotta_hopper_botany_pot').withChance(116),
+		Item.of('create:andesite_alloy').withChance(0.1),
+		'create:brass_ingot',
+		'minecraft:redstone',
+		'minecraft:redstone',
+		'minecraft:terracotta',
+		'minecraft:dried_kelp',
+		'minecraft:brick'
+	], 'botanypots:terracotta_botany_pot', [
+		event.recipes.createDeploying(inter, [inter, 'create:andesite_funnel']),
+		event.recipes.createDeploying(inter, [inter, 'create:brass_ingot']),
+		event.recipes.createDeploying(inter, [inter, 'minecraft:redstone']),
+		event.recipes.createDeploying(inter, [inter, 'minecraft:redstone']),
+		event.recipes.createDeploying(inter, [inter, 'create:tree_fertilizer']),
+	]).transitionalItem(inter).loops(1)
 })
